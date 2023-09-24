b0d5dd0acdc097aec37cb14f33ed886b1999e0ac
Naganjaneyulu G <87116202+GudditiNaganjaneyulu@users.noreply.github.com>
2023-09-24 08:22:06 +0000

    changed code

--- Ec2-vpc/sg.tf
+++ Ec2-vpc/sg.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "nginx" {
 
   ingress {
 
-    from_port   = 0
-    to_port     = 0
+    from_port   = 80
+    to_port     = 80
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     description = "allow all"
@@ -18,4 +18,36 @@ resource "aws_security_group" "nginx" {
     description = "allow all"
   }
 
+}
+
+resource "aws_security_group" "LB" {
+  vpc_id = aws_vpc.my-vpc.id
+  name   = "LB"
+
+  ingress {
+
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow all"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "allow all"
+  }
+
+}
+
+
+resource "aws_security_group_rule" "lb_to_nginxSG" {
+    type = "ingress"
+    from_port = 80
+    to_port = 80
+    source_security_group_id = aws_security_group.LB.id
+    security_group_id = aws_security_group.nginx.id
+  
 }
